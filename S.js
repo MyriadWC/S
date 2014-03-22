9c289a56a081d4ecff262226cbeadbfcd65c82de
tobylerone <tobylerone5@gmail.com>
2014-03-22 23:44:06 +0000

    fixed indentation

--- S.js
+++ S.js
@@ -6,40 +6,40 @@
 	
 	//the function that will get called from inside substrSplit
 	var _getSubstr = function(startingChar, charNum, array){
-        var string = "";
-        for(var i = startingChar; i < startingChar + charNum; i++){
-        	string += array[i];
-        }
-        //alert(string);
-        return string;
+	        var string = "";
+	        for(var i = startingChar; i < startingChar + charNum; i++){
+	        	string += array[i];
+	        }
+	        //alert(string);
+	        return string;
 	}
 
     //the function that will split a string into an array of 
     //substrings with a specified length
 	var _substrSplit = function(string, spacing){
-		var charArray = string.split('');
+    	var charArray = string.split('');
 		    
-   	    //the array containing the substrings
-	    var spacedArray = [];
-            
-	    //loop will create the array of substrings
-	    for(var i = spacing - 1, a = 0; i < charArray.length; i += spacing, a++){
-               spacedArray[a] = _getSubstr(i-(spacing-1), spacing, charArray);
-	    } 
-
-	    //if the character array wasn't perfectly 
-	    //divisible by the specified spacing, the 
-	    //last values are added to spacedArray here
-	    if(spacedArray.length * spacing < charArray.length){
-
-	    	//the spacing variable is redefined to be the remaining charArray values
-	    	spacing = charArray.length - spacedArray.length * spacing;
-
-	    	//these remaining values are turned into a string then pushed to the spacedArray
-	    	var remainingString = _getSubstr(charArray.length - (spacing),spacing,charArray);
-	    	spacedArray.push(remainingString);
+	   	//the array containing the substrings
+		var spacedArray = [];
+	            
+		//loop will create the array of substrings
+		for(var i = spacing - 1, a = 0; i < charArray.length; i += spacing, a++){
+	           spacedArray[a] = _getSubstr(i-(spacing-1), spacing, charArray);
+		} 
+	
+		//if the character array wasn't perfectly 
+		//divisible by the specified spacing, the 
+		//last values are added to spacedArray here
+		if(spacedArray.length * spacing < charArray.length){
+	
+			//the spacing variable is redefined to be the remaining charArray values
+		   	spacing = charArray.length - spacedArray.length * spacing;
+		
+		   	//these remaining values are turned into a string then pushed to the spacedArray
+		   	var remainingString = _getSubstr(charArray.length - (spacing),spacing,charArray);
+		   	spacedArray.push(remainingString);
 	    }
-	    return spacedArray;
+		return spacedArray;
 	}
     
     //the Fisher-Yates algorithm to shuffle an array
@@ -84,7 +84,7 @@
 		    }
 		}
     }
-    if (!s.noSpaces) {
+        if (!s.noSpaces) {
 		s.noSpaces = function(){
 			return this.replace(/\s/g, '');
 		}
@@ -93,15 +93,15 @@
 	if (!s.tidySpaces) {
 	    s.tidySpaces = function(){
 			return this.replace(/\s{2,}/g, ' ');
-		}
-    }
+	    }
+        }  
 
-    if (!s.scramble) {
+        if (!s.scramble) {
 	    s.scramble = function(){
 			var scrambled = fisherYates(this.split(''));
 			return scrambled.join('');
 		}
-    }
+        }
 
     if (!s.multiply) {
 	    s.multiply = function(multiplication){
@@ -113,4 +113,4 @@
 			return temp;
 		}
     }
-})()
+})();
